01909fa21bb7ebff74a5a201bdbc33a81d20db4e
Abinash Kumar Jena <51949985+abinash526@users.noreply.github.com>
2019-08-07 11:15:11 +0530

    Update UserController.java

--- src/main/java/com/stackroute/springBoot/controller/UserController.java
+++ src/main/java/com/stackroute/springBoot/controller/UserController.java
@@ -1,7 +1,6 @@
 package com.stackroute.springBoot.controller;
 
 import com.stackroute.springBoot.domain.Track;
-import com.stackroute.springBoot.exception.TrackNotFoundException;
 import com.stackroute.springBoot.repository.TrackRepository;
 import com.stackroute.springBoot.service.TrackService;
 import org.springframework.http.HttpStatus;
@@ -13,29 +12,26 @@ import java.util.List;
 @RestController
 @RequestMapping(value ="api/v1")
 public class UserController {
-    //@Autowired
     TrackService trackService;
-    TrackRepository trackRepository;
-
+    @Autowired
     public UserController(TrackService trackService) {
         this.trackService = trackService;
     }
     @PostMapping("track")
-    public ResponseEntity<?> saveuser(@RequestBody Track track)throws RuntimeException{
+    public ResponseEntity<?> saveuser(@RequestBody Track track){
 
         ResponseEntity responseEntity;
-
+        try {
             trackService.saveUser(track);
             responseEntity=new ResponseEntity<String>("Successfully Created", HttpStatus.CREATED);
 
-
+        }catch (Exception e){
+            responseEntity=new ResponseEntity<String>(e.getMessage(),HttpStatus.CONFLICT);
+        }
         return responseEntity;
     }
     @GetMapping("user")
     public ResponseEntity<?> getAllUsers(){
-        //System.out.println(trackRepository.findByFirstName("Abinash"));
-        System.out.println(trackService.getByTrackName("Nice").toString());
-        System.out.println(trackService.getTrackByNameSortByName("Nice").toString());
 
         return new ResponseEntity<List<Track>>(trackService.getAllUsers(),HttpStatus.OK);
     }
@@ -43,19 +39,22 @@ public class UserController {
     public ResponseEntity<?> updateUser(@RequestBody Track track)
     {
         ResponseEntity responseEntity;
-
+        try
+        {
             trackService.saveUser(track);
             responseEntity=new ResponseEntity<String>("Succesfully Updated",HttpStatus.CREATED);
-
-
+        }
+        catch(Exception e)
+        {
+            responseEntity=new ResponseEntity<String>(e.getMessage(),HttpStatus.CONFLICT);
+        }
         return responseEntity;
     }
     @DeleteMapping(value = "/delete/{id}")
-    public ResponseEntity<?> deleteUser(@PathVariable int id) throws RuntimeException
+    public ResponseEntity<?> deleteUser(@PathVariable int id)
     {
         ResponseEntity responseEntity;
-
-            trackService.deleteUser(id);
+        trackService.deleteUser(id);
         responseEntity=new ResponseEntity<String>("Succesfully deleted",HttpStatus.NO_CONTENT);
         return responseEntity;
 
