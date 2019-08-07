9bc32eb0215f0922d4e506f054621df144e9b554
Abinash Kumar Jena <51949985+abinash526@users.noreply.github.com>
2019-08-07 11:09:37 +0530

    Update MuzixAppApplication.java

--- src/main/java/com/stackroute/springBoot/MuzixAppApplication.java
+++ src/main/java/com/stackroute/springBoot/MuzixAppApplication.java
@@ -1,1 +1,26 @@
+package com.stackroute.springBoot;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@SpringBootApplication
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan
+public class MuzixAppApplication {
+	/*@RequestMapping("/")
+	String home(){
+		return "Hello World" +
+				"How are you";
+	}*/
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(MuzixAppApplication.class,args);
+
+		//System.out.println(new DemoApplication().userRepository.findByFirstName("Abinash"));
+	}
+
+}
