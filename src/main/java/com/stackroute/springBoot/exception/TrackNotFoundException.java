6375ba0c1a04d450def60c56e70c525531d61bb2
Abinash Kumar Jena <51949985+abinash526@users.noreply.github.com>
2019-08-07 15:49:45 +0530

    Create TrackNotFoundException.java

---
+++ src/main/java/com/stackroute/springBoot/exception/TrackNotFoundException.java
@@ -0,0 +1,21 @@
+package com.stackroute.springBoot.exception;
+
+
+
+public class TrackNotFoundException extends Exception {
+
+    private String message;
+
+
+
+    public TrackNotFoundException(){}
+
+    public TrackNotFoundException(String message) {
+
+        super(message);
+
+        this.message = message;
+
+    }
+
+}
