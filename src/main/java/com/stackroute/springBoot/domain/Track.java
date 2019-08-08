93a0be3e3f8af2080d1512e850755aa67e9597ac
Abinash Kumar Jena <51949985+abinash526@users.noreply.github.com>
2019-08-08 06:29:31 +0530

    Update Track.java

--- src/main/java/com/stackroute/springBoot/domain/Track.java
+++ src/main/java/com/stackroute/springBoot/domain/Track.java
@@ -7,8 +7,8 @@ import javax.persistence.Id;
 public class Track {
     @Id
     int id;
-    String name;
-    String comment;
+    private String name;
+    private String comment;
 
     @Override
     public String toString() {
